1c510e91505c4ac2e47dac7392d6a15a42ec5a69
EC2 Default User <ec2-user@ip-172-31-1-134.ap-northeast-1.compute.internal>
2022-12-13 15:00:12 +0000

    [work]error display

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
